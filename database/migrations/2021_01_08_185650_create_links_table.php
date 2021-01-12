9bbf78dc414cdb055602c3eec61c288667a9b55a
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-01-12 18:22:43 -0300

    Ajustando migrate q tava c unsigned big integer

--- database/migrations/2021_01_08_185650_create_links_table.php
+++ database/migrations/2021_01_08_185650_create_links_table.php
@@ -16,7 +16,7 @@ class CreateLinksTable extends Migration
         Schema::create('links', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDeleteCascade();
-            $table->unsignedBigInteger('category_id')->references('id')->on('categories')->onDeleteCascade();
+            $table->foreignId('category_id')->references('id')->on('categories')->onDeleteCascade();
             $table->string('link');
             $table->string('name')->nullable();
             $table->timestamps();
